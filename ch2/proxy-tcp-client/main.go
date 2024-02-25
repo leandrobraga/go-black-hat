f8acffeb835d7e296770e2b14aa069fbfaa8dfb3
Leandro Braga <leandro.quadros@gmail.com>
2024-02-25 23:01:03 +0000

    proxy tcp client

---
+++ ch2/proxy-tcp-client/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func handle(src net.Conn) {
+	dst, err := net.Dial("tcp", "joescatcam.website:80")
+	if err != nil {
+		log.Fatalln("Unable to connect to our unreachable host")
+	}
+
+	defer dst.Close()
+
+	// Run in gourotine to prevent io.Copy from blocking
+	go func() {
+		// Copy our source's output to the destination
+		if _, err := io.Copy(dst, src); err != nil {
+			log.Fatalln(err)
+		}
+	}()
+}
+
+func main() {
+	// Listen on local port 80
+	listener, err := net.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatalln("Unable to bind port 80")
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Fatalln("Unable to accept connection")
+		}
+		go handle(conn)
+	}
+}
